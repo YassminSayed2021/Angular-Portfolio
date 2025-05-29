53c669cd968bb521b784c48bfa2b977c4a44b7f8
Yassmin sayed <yassmin.sayed868@gmail.com>
2025-05-29 23:00:10 +0300

    Angular Portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { AboutSectionComponent } from "./about-section/about-section.component";
+import { SkillsSectionComponent } from "./skills-section/skills-section.component";
+import { PortfolioSectionComponent } from "./portfolio-section/portfolio-section.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSectionComponent, AboutSectionComponent, SkillsSectionComponent, PortfolioSectionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
